5b90aa9e4a04f872b1925256dff3084c54738fcb
dmtrllv <dev.dmtrllv@gmail.com>
2021-01-18 19:23:44 +0100

    fixed wrong implemented method

--- template-project/src/server/index.ts
+++ template-project/src/server/index.ts
@@ -2,7 +2,7 @@ import { Server } from "ion/server";
 
 export default class extends Server
 {
-	public onStart()
+	public onListening()
 	{
 		console.log(`server listening on http://${this.host}:${this.port}`);
 	}
