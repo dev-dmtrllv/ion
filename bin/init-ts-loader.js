7dfa2c2e5d1677119fa3971d636bafafb27a31b6
dmtrllv <dev.dmtrllv@gmail.com>
2021-01-18 19:44:33 +0100

    try fixingpart 2

--- bin/init-ts-loader.js
+++ bin/init-ts-loader.js
@@ -4,7 +4,30 @@ exports.initTsLoader = void 0;
 const babel_config_1 = require("./config/babel-config");
 const override_require_1 = require("./override-require");
 const initTsLoader = () => {
-    require("@babel/register")(babel_config_1.createBabelConfig("server"));
+    require("@babel/register")({
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+        cache: true,
+        root: process.cwd(),
+        ignore: [
+            /node_modules/,
+            "./package-lock.json",
+            "./package.json",
+            /\.s?(c|a)ss$/
+        ],
+        presets: [
+            ["@babel/preset-env", { targets: { node: true } }],
+            "@babel/preset-typescript",
+            "@babel/preset-react",
+        ],
+        plugins: [
+            "module-resolver", { root: process.cwd(), alias: babel_config_1.getBabelAliases() },
+            "@babel/plugin-syntax-dynamic-import",
+            ["@babel/plugin-proposal-decorators", { "legacy": true }],
+            ["@babel/plugin-proposal-class-properties", { "loose": true }],
+            "@babel/plugin-proposal-object-rest-spread",
+            "transform-es2015-modules-commonjs",
+        ],
+    });
     override_require_1.overrideRequire();
 };
 exports.initTsLoader = initTsLoader;
--- cli/src/init-ts-loader.ts
+++ cli/src/init-ts-loader.ts
@@ -1,8 +1,31 @@
-import { createBabelConfig } from "./config/babel-config";
+import { getBabelAliases } from "./config/babel-config";
 import { overrideRequire } from "./override-require";
 
 export const initTsLoader = () =>
 {
-	require("@babel/register")(createBabelConfig("server"));
+	require("@babel/register")({
+		extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+		cache: true,
+		root: process.cwd(),
+		ignore: [
+			/node_modules/,
+			"./package-lock.json",
+			"./package.json",
+			/\.s?(c|a)ss$/
+		],
+		presets: [
+			["@babel/preset-env", { targets: { node: true } }],
+			"@babel/preset-typescript",
+			"@babel/preset-react",
+		],
+		plugins: [
+			"module-resolver", { root: process.cwd(), alias: getBabelAliases() },
+			"@babel/plugin-syntax-dynamic-import",
+			["@babel/plugin-proposal-decorators", { "legacy": true }],
+			["@babel/plugin-proposal-class-properties", { "loose": true }],
+			"@babel/plugin-proposal-object-rest-spread",
+			"transform-es2015-modules-commonjs",
+		],
+	});
 	overrideRequire();
 }
